fcb457fa21542a78e780698dab33d0847a553f6e
dalud <jaakko.niska@gmail.com>
2018-09-12 17:18:13 +0300

    kävelyä aloitettu

--- sus.ino
+++ sus.ino
@@ -1,15 +1,15 @@
 //määritellään pinnit raajojen niveliin
-#define EOY 1 //EtuOikeaYlänivel
-#define EOA 2 //EtuOikeaAla
-#define EVY 3 //EtuVasen...
-#define EVA 4
+#define EOY 0 //EtuOikeaYlänivel
+#define EOA 1 //EtuOikeaAla
+#define EVY 2 //EtuVasen...
+#define EVA 3
 
-#define TOY 5 //TakaOikeaYlä
-#define TOK 6 //TakaOikeaKeski
-#define TOA 7 //TakaOikeaAlanivel
-#define TVY 8 //TakaVasen...
-#define TVK 9
-#define TVA 10
+#define TOY 4 //TakaOikeaYlä
+#define TOK 5 //TakaOikeaKeski
+#define TOA 6 //TakaOikeaAlanivel
+#define TVY 7 //TakaVasen...
+#define TVK 8
+#define TVA 9
 
 const int LKM = 10; //nivelten lukumäärä
 
@@ -28,8 +28,40 @@ void setup() {
     pinMode(nivelet[i], OUTPUT);
   }  
 }
+//alirutiinit raajoille
+void etu(char puoli){
+    switch(puoli){
+    case 'O':
+      digitalWrite(EOA, HIGH);
+      delay(dA);
+      digitalWrite(EOY, HIGH);
+      digitalWrite(EOA, LOW);
+      delay(dY);
+      digitalWrite(EOY, LOW);
+      break;
+    case 'V':
+      digitalWrite(EVA, HIGH);
+      delay(dA);
+      digitalWrite(EVY, HIGH);
+      digitalWrite(EVA, LOW);
+      delay(dY);
+      digitalWrite(EVY, LOW);
+      break;
+  }
+}
+
+void taka(char puoli){
+  
+}
+
+//kävelysykli
+void kavely(){
+  etu('O');
+  taka('V');
+  etu('V');
+  taka('O');
+}
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  kavely();
 }
