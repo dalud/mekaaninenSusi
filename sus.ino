7fdc35c8d800d4686732d0bc2a379f2e3b145d9d
dalud <jaakko.niska@gmail.com>
2018-09-12 10:02:24 +0300

    initial commit

---
+++ sus.ino
@@ -0,0 +1,22 @@
+//määritellään pinnit raajojen niveliin
+#define EOY 1 //EtuOikeaYlänivel
+#define EOA 2 //EtuOikeaAla
+#define EVY 3 //EtuVasen...
+#define EVA 4
+
+#define TOY 5 //TakaVasenYlä
+#define TOK 6 //TakaVasenKeski
+#define TOA 7 //TakaVasenAlanivel
+#define TVY 8 //TakaVasen...
+#define TVK 9
+#define TVA 10
+
+void setup() {
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
